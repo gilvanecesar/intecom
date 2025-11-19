38c3fea2e8afb45c7b0fc69c48142b68be33dbb7
Gilvane César <gilvanecesar@MacBook-Pro-de-Gilvane.local>
2025-11-19 14:30:25 -0300

    feat: backend incluido - python

--- backend/routes/mecanismo.py
+++ backend/routes/mecanismo.py
@@ -20,3 +20,32 @@ def criar_item(item: Mecanismo, session: Session = Depends(get_session)):
 def listar_itens(session: Session = Depends(get_session)):
     consulta = select(Mecanismo)
     return session.exec(consulta).all()
+
+@router.put("/mecanismo/{item_id}")
+def atualizar_item(item_id: int, dados: Mecanismo, session: Session = Depends(get_session)):
+    item_db = session.get(Mecanismo, item_id)
+
+    if not item_db:
+        return {"erro": "Item não encontrado"}
+    
+    item_db.periodo = dados.periodo
+    item_db.produzido = dados.produzido
+    item_db.meta = dados.meta
+    item_db.diferenca = dados.diferenca
+
+    session.commit()
+    session.refresh(item_db)
+
+    return item_db
+
+@router.delete("/mecanismo/{item_id}")
+def deletar_item(item_id: int, session: Session = Depends(get_session)):
+    item_db = session.get(Mecanismo, item_id)
+
+    if not item_db:
+        return{"erro": "Item não encontrado"}
+    
+    session.delete(item_db)
+    session.commit()
+
+    return {"mensagem": "Item removido com sucesso"}
