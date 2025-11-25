14202139b03fcb8e597f21aa56c765cd1e1f7109
Gilvane César <gilvanecesar@MacBook-Pro-de-Gilvane.local>
2025-11-25 13:17:15 -0300

    feat: Entregando parte do dashboard

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,51 +1,54 @@
-import './dashboard.style.css';
-import React from 'react';
-import { FaIndustry } from 'react-icons/fa';
-
+import "./dashboard.style.css";
+import { CiCalendarDate } from "react-icons/ci";
+import { FaIndustry } from "react-icons/fa";
 
 export default function Dashboard() {
-  return( 
-   <>
+  return (
+    <>
       <div className="card-titulo">
         <h1>Cards Indicadores</h1>
       </div>
 
-
       {/* container principal */}
       <div className="container">
-      
         {/* CARD 1 */}
         <div className="card-1">
-            <div className='card-1-interno'>
-              <FaIndustry className='fa-ind' />
-              <h2>Peças por dia</h2>
-            </div>
-        </div>
-              
-          {/* CARD 02     */}
-          <div className="card-2">
-              <h2>Dias trabalhados</h2>
-
+          <div className="card-1-interno">
+            <FaIndustry className="fa-ind" />
+            <h2>Peças por dia</h2>
           </div>
-              
+        </div>
+
+        {/* CARD 02     */}
+        <div className="card-2">
+          <div className="card-2-interno">
+            <CiCalendarDate className="fa-ind" />
+            <h2>Dias trabalhados</h2>
+          </div>
+        </div>
+
         {/* CARD 03 */}
         <div className="card-3">
-          <FaIndustry className='fa-ind' />
-          <h2>Peças por dia</h2>
+          <div className="card-3-interno">
+            <CiCalendarDate className="fa-ind" />
+            <h2>Dias trabalhados</h2>
+          </div>
         </div>
 
         {/* CARD 04     */}
         <div className="card-4">
-          <h2>Dias trabalhados</h2>
-
+          <div className="card-4-interno">
+            <FaIndustry className="fa-ind" />
+            <h2>Dias trabalhados</h2>
+          </div>
         </div>
       </div>
-      <section className='section-container'>
-        <div className='cnt-section'>
+
+      <section className="section-container">
+        <div className="cnt-section">
           <h1>AQUI VAI OS INDICADORES</h1>
         </div>
-
       </section>
     </>
-  )
+  );
 }
